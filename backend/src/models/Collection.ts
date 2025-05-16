28d333233791b89bd72d1331edac9bb9039fada2
angelasandom <angelasandom@gmail.com>
2025-05-16 08:16:57 +0100

    Update controllers, routes, services, types

--- backend/src/models/Collection.ts
+++ backend/src/models/Collection.ts
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 const ArtworkSchema = new mongoose.Schema({
-    apiSource: { tyoe: String, enum: ['cleveland', 'harvard'], required: true },
     id: { type: String, required: true },
     title: { type: String, required: true },
+    creator: { type: String },
     imageUrl: { type: String, required: true },
     culture: String,
     technique: String,
+    apiSource: { type: String, enum: ['The Cleveland Museum of Art', 'Harvard Art Museums'], required: true }
 });
 
 const CollectionSchema = new mongoose.Schema({
