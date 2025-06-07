1ba681867741070c0e1641ef1220b5face74ab47
angelasandom <angelasandom@gmail.com>
2025-06-07 01:21:09 +0100

    Fix error when deployment

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -4,7 +4,6 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
-import Navbar from "../components/Navbar";
 
 const Register = () => {
   const [email, setEmail] = useState("");
